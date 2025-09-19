d8ceaedd7e72a0ab495aa907b06ad0329b2fdf0f
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-09-19 18:03:03 -0500

    Add fetch pokemons

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,17 @@
-export default function PokemonsPage() {
+const getPokemons = async (limit = 20, offset = 0) => {
+ const data = await fetch(
+  `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+ ).then((res) => res.json());
+
+ return data;
+};
+
+export default async function PokemonsPage() {
+ const pokemons = await getPokemons();
+
  return (
   <div>
-   <h1>Pokemons Page</h1>
+   <h1>{JSON.stringify(pokemons)}</h1>
   </div>
  );
 }
