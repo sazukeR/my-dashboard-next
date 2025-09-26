05652ad760326e3a2d8b87c3519929e731a44713
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-09-26 18:44:32 -0500

    Add pokemon page http request

--- src/app/dashboard/pokemons/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemons/pokemon/[id]/page.tsx
@@ -1,11 +1,27 @@
+import { Pokemon } from "../../interface/pokemon.interface";
+
 interface Props {
- params: { id: string };
+ params: Promise<{ id: string }>;
 }
 
-export default function PokemonPage({ params: { id } }: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+ const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+  cache: "force-cache",
+ }).then((res) => res.json());
+
+ return resp;
+};
+
+export default async function PokemonPage({ params }: Props) {
+ const { id } = await params;
+
+ const pokemon = await getPokemon(id);
+
  return (
   <div>
    <h1>{id}</h1>
+
+   <p>{JSON.stringify(pokemon)}</p>
   </div>
  );
 }
